50528ae0eb2d386c59ba98a6f89c9d72a2dcd58c
Naseef Nusky <naseefnusky09@gmail.com>
2025-09-25 10:32:40 +0530

    layout issue fixed

--- src/App.js
+++ src/App.js
@@ -65,7 +65,6 @@ function App() {
                 <AccordionFAQ />
                 <TestimonialSlider />
                 <ContactFormSection />
-                <Footer />
               </main>
             }
           />
